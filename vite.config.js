7a7c58a8c6a5beeeb8c9cab2052a69e60c12695a
hanyong5 <hanyong5@naver.com>
2025-05-15 14:10:49 +0900

    modi

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/rest": {
-        target: "https://jejuckl.kr",
-        changeOrigin: true,
-        secure: false,
-        // 필요시 아래 주석 해제
-        // rewrite: (path) => path.replace(/^\/rest/, '/rest'),
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/rest": {
+  //       target: "https://jejuckl.kr",
+  //       changeOrigin: true,
+  //       secure: false,
+
+  //     },
+  //   },
+  // },
 });
