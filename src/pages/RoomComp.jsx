cdc8972e48f2675a2b1c3c6de4af45a3d06c54de
hanyong5 <hanyong5@naver.com>
2025-05-19 17:22:45 +0900

    modi#4

--- src/pages/RoomComp.jsx
+++ src/pages/RoomComp.jsx
@@ -33,8 +33,7 @@ function RoomComp() {
   // 예약 데이터 불러오기
   const fetchBooking = async () => {
     try {
-      const baseUrl = import.meta.env.DEV ? "" : "https://jejuckl.kr";
-      const url = `${baseUrl}/rest/booking/list?type=BOO_TYPE002&strDate=${dateStr}`;
+      const url = `/rest/booking/list?type=BOO_TYPE002&strDate=${dateStr}`;
       const res = await axios.get(url, {
         headers: { Accept: "application/xml" },
       });
